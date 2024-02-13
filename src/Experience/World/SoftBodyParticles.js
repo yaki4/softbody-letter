468e75264fc62cbf43a45d162503040aa35ef995
MisterPrada <lesha.skorpion@gmail.com>
2024-02-13 15:29:27 +0300

    Refactored SoftBodyParticles

--- src/Experience/World/SoftBodyParticles.js
+++ src/Experience/World/SoftBodyParticles.js
@@ -26,36 +26,72 @@ export default class SoftBodyParticles {
     }
 
     postInit() {
-        const e = this.particlesSim.textureSize.x, t = this.particlesSim.textureSize.y, i = e * t;
-        this.particlesDataForTexture = new Float32Array(i * 4), this.particlesSoftBodyPositionDataTexture = this.fboHelper.createDataTexture(this.particlesDataForTexture, e, t, !0, !0), this.positionRenderTarget = this.fboHelper.createRenderTarget(1, 1, !0, !0), this.positionRenderTarget.setSize(e, t), this.fboHelper.copy(this.particlesSoftBodyPositionDataTexture, this.positionRenderTarget), this.baryWeightsArray = new Float32Array(i * 4), this.tetsUvX = new Float32Array(i * 4), this.tetsUvY = new Float32Array(i * 4);
-        let n = 0;
-        for (let r = 0; r < this.particleCount; r++) {
-            const a = this.baryWeights[n++], l = this.baryWeights[n++], u = this.baryWeights[n++], c = 1 - a - l - u;
-            this.baryWeightsArray[4 * r] = a, this.baryWeightsArray[4 * r + 1] = l, this.baryWeightsArray[4 * r + 2] = u, this.baryWeightsArray[4 * r + 3] = c;
-            let f = this.tetIndices[r] * 4;
-            for (let p = 0; p < 4; p++) {
-                const _ = this.softBodyTets.tetIds[f];
-                this.tetsUvX[4 * r + p] = (_ % this.softBodyTets.posTextureSize.x + .5) / this.softBodyTets.posTextureSize.x, this.tetsUvY[4 * r + p] = (Math.floor(_ / this.softBodyTets.posTextureSize.x) + .5) / this.softBodyTets.posTextureSize.y, f++
+        const textureWidth = this.particlesSim.textureSize.x;
+        const textureHeight = this.particlesSim.textureSize.y;
+        const totalParticles = textureWidth * textureHeight;
+
+        this.particlesDataForTexture = new Float32Array( totalParticles * 4 );
+        this.particlesSoftBodyPositionDataTexture = this.fboHelper.createDataTexture( this.particlesDataForTexture, textureWidth, textureHeight, true, true );
+        this.positionRenderTarget = this.fboHelper.createRenderTarget( 1, 1, true, true );
+        this.positionRenderTarget.setSize( textureWidth, textureHeight );
+        this.fboHelper.copy( this.particlesSoftBodyPositionDataTexture, this.positionRenderTarget );
+        this.baryWeightsArray = new Float32Array( totalParticles * 4 );
+        this.tetsUvX = new Float32Array( totalParticles * 4 );
+        this.tetsUvY = new Float32Array( totalParticles * 4 );
+
+        let weightIndex = 0;
+
+        for ( let particleIndex = 0; particleIndex < this.particleCount; particleIndex++ ) {
+            const weight1 = this.baryWeights[ weightIndex++ ];
+            const weight2 = this.baryWeights[ weightIndex++ ];
+            const weight3 = this.baryWeights[ weightIndex++ ];
+            const weight4 = 1 - weight1 - weight2 - weight3;
+
+            this.baryWeightsArray[ 4 * particleIndex ] = weight1;
+            this.baryWeightsArray[ 4 * particleIndex + 1 ] = weight2;
+            this.baryWeightsArray[ 4 * particleIndex + 2 ] = weight3;
+            this.baryWeightsArray[ 4 * particleIndex + 3 ] = weight4;
+
+            let tetIndex = this.tetIndices[ particleIndex ] * 4;
+
+            for ( let i = 0; i < 4; i++ ) {
+                const tetId = this.softBodyTets.tetIds[ tetIndex ];
+                this.tetsUvX[ 4 * particleIndex + i ] = ( tetId % this.softBodyTets.posTextureSize.x + 0.5 ) / this.softBodyTets.posTextureSize.x;
+                this.tetsUvY[ 4 * particleIndex + i ] = ( Math.floor( tetId / this.softBodyTets.posTextureSize.x ) + 0.5 ) / this.softBodyTets.posTextureSize.y;
+                tetIndex++;
             }
         }
-        this.tetsUvXTexture = this.fboHelper.createDataTexture(this.tetsUvX, e, t, !0, !0), this.tetsUvYTexture = this.fboHelper.createDataTexture(this.tetsUvY, e, t, !0, !0), this.baryWeightsTexture = this.fboHelper.createDataTexture(this.baryWeightsArray, e, t, !0, !0), this.softBodyPositionMaterial = new THREE.RawShaderMaterial({
+
+        this.tetsUvXTexture = this.fboHelper.createDataTexture( this.tetsUvX, textureWidth, textureHeight, true, true );
+        this.tetsUvYTexture = this.fboHelper.createDataTexture( this.tetsUvY, textureWidth, textureHeight, true, true );
+        this.baryWeightsTexture = this.fboHelper.createDataTexture( this.baryWeightsArray, textureWidth, textureHeight, true, true );
+
+        this.softBodyPositionMaterial = new THREE.RawShaderMaterial( {
             uniforms: {
                 u_time: this.properties.sharedUniforms.u_time,
                 u_deltaTime: this.properties.sharedUniforms.u_deltaTime,
-                u_tetsTexture: {value: null},
-                u_tetsUvXTexture: {value: this.tetsUvXTexture},
-                u_tetsUvYTexture: {value: this.tetsUvYTexture},
-                u_tetsTextureSize: {value: new THREE.Vector2},
-                u_baryWeightsTexture: {value: this.baryWeightsTexture}
-            }, vertexShader: this.fboHelper.vertexShader, fragmentShader: this.fboHelper.precisionPrefix + particleSoftBodySim
-        })
+                u_tetsTexture: { value: null },
+                u_tetsUvXTexture: { value: this.tetsUvXTexture },
+                u_tetsUvYTexture: { value: this.tetsUvYTexture },
+                u_tetsTextureSize: { value: new THREE.Vector2() },
+                u_baryWeightsTexture: { value: this.baryWeightsTexture }
+            },
+            vertexShader: this.fboHelper.vertexShader,
+            fragmentShader: this.fboHelper.precisionPrefix + particleSoftBodySim
+        } );
     }
 
     computeVisualData() {
-        this.particlesPos = this.properties.pointsGeometry.attributes.position.array.slice(), this.particlesDefaultPos = this.particlesPos.slice(), this.particleCount = this.particlesPos.length / 3, this.tetIndices = this.properties.pointsGeometry.attributes.tet.array, this.baryWeights = this.properties.pointsGeometry.attributes.bary.array
+        this.particlesPos = this.properties.pointsGeometry.attributes.position.array.slice()
+        this.particlesDefaultPos = this.particlesPos.slice()
+        this.particleCount = this.particlesPos.length / 3
+        this.tetIndices = this.properties.pointsGeometry.attributes.tet.array
+        this.baryWeights = this.properties.pointsGeometry.attributes.bary.array
     }
 
-    endFrame(e) {
-        this.softBodyPositionMaterial.uniforms.u_tetsTexture.value = this.softBodyTets.posTexture, this.softBodyPositionMaterial.uniforms.u_tetsTextureSize.value.copy(this.softBodyTets.posTextureSize), this.fboHelper.render(this.softBodyPositionMaterial, this.positionRenderTarget)
+    endFrame( delta ) {
+        this.softBodyPositionMaterial.uniforms.u_tetsTexture.value = this.softBodyTets.posTexture
+        this.softBodyPositionMaterial.uniforms.u_tetsTextureSize.value.copy( this.softBodyTets.posTextureSize )
+        this.fboHelper.render( this.softBodyPositionMaterial, this.positionRenderTarget )
     }
 }
